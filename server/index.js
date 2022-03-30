cea256b970c756d45572bad4919c1862856da39b
katharotiyarajnish <katharotiyarajnish@gmail.com>
2022-03-30 10:41:47 -0400

    Added product model

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@ const bodyParse = require("body-parser");
 const cors = require('cors');
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:8001'
-};
-
+const corsOptions = {origin: 'http://localhost:8001'};
 app.use(cors(corsOptions));
-
-//const db = require("./app/models");
-
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: true }));
 
+
+const db = require("./models");
+
+//Establisting Connection
+db.connex.sync()
+
+
 app.get('/', (req, res) => res.json({ message: "Welcome" }))
 
-const PORT = process.env.PORT || 8080;
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on " + PORT))
